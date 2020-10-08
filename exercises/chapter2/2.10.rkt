11d0dafde37a228b7942ecb36ebe1b92117448f7
vaziliybober <vaziliybober@gmail.com>
2020-10-08 22:53:22 +0300

    do 2.7 - 2.10

---
+++ exercises/chapter2/2.10.rkt
@@ -0,0 +1,47 @@
+ 
+ #lang sicp
+(#%require rackunit) 
+
+(define (make-interval a b) (cons a b))
+
+(define (lower-bound interval) (car interval))
+(define (upper-bound interval) (cdr interval))
+
+(define (add-interval x y)
+  (make-interval (+ (lower-bound x) (lower-bound y))
+                 (+ (upper-bound x) (upper-bound y))))
+
+(define (sub-interval x y)
+  (make-interval (- (lower-bound x) (upper-bound y))
+                 (- (upper-bound x) (lower-bound y))))
+
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+        (p2 (* (lower-bound x) (upper-bound y)))
+        (p3 (* (upper-bound x) (lower-bound y)))
+        (p4 (* (upper-bound x) (upper-bound y))))
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
+
+(define (div-interval x y)
+  (if (and (<= (lower-bound y) 0)
+           (>= (upper-bound y) 0))
+      (error "division by zero!")
+      (mul-interval
+       x
+       (make-interval (/ 1.0 (upper-bound y))
+                      (/ 1.0 (lower-bound y))))))
+
+(define (width x)
+  (/ (- (upper-bound x)
+        (lower-bound x))
+     2))
+
+(define i (make-interval 3 5))
+(check-equal? (lower-bound i) 3)
+(check-equal? (upper-bound i) 5)
+
+(define i2 (make-interval -3 -1))
+(check-equal? (sub-interval i i2) (make-interval 4 8)) 
+
+(check-equal? (width i) 1)
---
+++ exercises/chapter2/2.7.rkt
@@ -0,0 +1,29 @@
+ #lang sicp
+(#%require rackunit) 
+
+(define (make-interval a b) (cons a b))
+
+(define (lower-bound interval) (car interval))
+(define (upper-bound interval) (cdr interval))
+
+(define (add-interval x y)
+  (make-interval (+ (lower-bound x) (lower-bound y))
+                 (+ (upper-bound x) (upper-bound y))))
+
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+        (p2 (* (lower-bound x) (upper-bound y)))
+        (p3 (* (upper-bound x) (lower-bound y)))
+        (p4 (* (upper-bound x) (upper-bound y))))
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
+
+(define (div-interval x y)
+  (mul-interval
+   x
+   (make-interval (/ 1.0 (upper-bound y))
+                  (/ 1.0 (lower-bound y)))))
+
+(define i (make-interval 3 5))
+(check-equal? (lower-bound i) 3)
+(check-equal? (upper-bound i) 5)
